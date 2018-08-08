ee57b5684f2b2bccbf6d245d51b5c4f04620e0d8
melfarissi <mohamed.elfarissi11@gmail.com>
2018-08-08 16:24:44 +0100

    first commit

---
+++ client/main.js
@@ -0,0 +1,21 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+// Create a MongoDB Collection
+Gpsdata = new Mongo.Collection("gpsdata");
+
+// Inserting data before executing Gpsdata.find().fetch() instruction
+//Meteor.call('insertGPSData');
+
+Template.PhidgetGPS.helpers({
+  'data': function(){
+      return Gpsdata.find().fetch();
+  }
+});
+
+// 'updatingGPSData' function is called each 3 seconds.
+/*setInterval(function() {
+  Meteor.call('updatingGPSData');
+}, 3000);*/
