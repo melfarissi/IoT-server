5933be18850f581373a61c7b683f2a865b7f5183
melfarissi <mohamed.elfarissi11@gmail.com>
2018-08-09 19:32:18 +0100

    Adding clients data into the database and creating two markers for Google Map data visualization

--- client/main.js
+++ client/main.js
@@ -3,11 +3,17 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-// Create a MongoDB Collection
+// Create a MongoDB Collection for client1
 Gpsdata = new Mongo.Collection("gpsdata");
 
+// Create a MongoDB Collection for client2
+//Gpsdata2 = new Mongo.Collection("gpsdata2");
+
 // Inserting data before executing Gpsdata.find().fetch() instruction
-//Meteor.call('insertGPSData');
+Meteor.call('insertGPSData');
+
+//Meteor.call('insertGPSData2');
+
 
 Template.PhidgetGPS.helpers({
   'data': function(){
@@ -15,7 +21,13 @@ Template.PhidgetGPS.helpers({
   }
 });
 
-// 'updatingGPSData' function is called each 3 seconds.
+/*Template.PhidgetGPS2.helpers({
+  'data2': function(){
+      return Gpsdata2.find().fetch();
+  }
+});*/
+
+// 'updatingGPSData1' function is called each 3 seconds.
 /*setInterval(function() {
-  Meteor.call('updatingGPSData');
+  Meteor.call('updatingGPSData1');
 }, 3000);*/
--- server/main.js
+++ server/main.js
@@ -2,10 +2,14 @@
 import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
+
 });
 
-// Create a MongoDB Collection
+// Create a MongoDB Collection for client1
 Gpsdata = new Mongo.Collection("gpsdata");
+
+// Create a MongoDB Collection for client2
+//Gpsdata2 = new Mongo.Collection("gpsdata2");
 
 const WebSocket = require('ws');
 
@@ -14,13 +18,39 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         tab = message.split(',');
-          if (tab[2] == 'c3'){
-              console.log('Client3 | latitude %s, longitude %s', tab[0], tab[1]);
+          if (tab[2] == 'c1'){
+              tab1 = [tab[0], tab[1]];
+              console.log('Client1 | latitude %s, longitude %s', tab1[0], tab1[1]);
           }
-          if (tab[2] == 'c4'){
-              console.log('Client4 | latitude %s, longitude %s', tab[0], tab[1]);
+          if (tab[2] == 'c2'){
+              tab2 = [tab[0], tab[1]];
+              console.log('Client2 | latitude %s, longitude %s', tab2[0], tab2[1]);
           }
+
   });
 
   ws.send('Connected');
 });
+
+  Meteor.methods({
+      'insertGPSData': function(){
+          Gpsdata.insert({
+              Latitude1: tab1[0],
+              Longitude1: tab1[1],
+              Latitude2: tab2[0],
+              Longitude2: tab2[1],
+          });
+          },
+      'updatingGPSData': function(){
+          // To change the values of a document without deleting it first, we need to use the $set operator.
+          // This operator will change the value of specified fields.
+          Gpsdata.update(Gpsdata.findOne({})._id,{$set: {Latitude1: tab1[0]}});
+          Gpsdata.update(Gpsdata.findOne({})._id,{$set: {Longitude1: tab1[1]}});
+        }/*,
+      'insertGPSData2': function(){
+          Gpsdata2.insert({
+              Latitude: tab2[0],
+              Longitude: tab2[1],
+          });
+        }*/
+  });
